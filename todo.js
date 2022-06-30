06c4297adfa917033948856f65f379f3b33f9a6a
Edu Mora <100218106+edumorac@users.noreply.github.com>
2022-06-29 23:24:56 -0400

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,6 +1,11 @@
-function Todo({todo, index, remove}){
-    function handle(){
-        remove(index)
-    }
-    return <div className="todo" onClick={handle}>{todo.text} (-)</div>
+function Todo({ todo, index, remove }) {
+  function handle() {
+    console.log("Ping:", index);
+    remove(index);
+  }
+  return (
+    <div className="todo" onClick={handle}>
+      {todo.text} (-)
+    </div>
+  );
 }
