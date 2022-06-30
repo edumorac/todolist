bed1f9faa0e8cdd6dd256607055f73613f4a59de
Edu Mora <100218106+edumorac@users.noreply.github.com>
2022-06-29 23:23:21 -0400

    Update form.js

--- form.js
+++ form.js
@@ -1,21 +1,21 @@
 function TodoForm({addTodo}){
-    const [value, setValue] = React.useState('');
+  const [value,setValue] = React.useState('');
+  
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(!value) return;
+    addTodo(value);
+    setValue('');
+  }
 
-    const handleSubmit = e => {
-        e.preventDefault();
-        if (!value) return;
-        addTodo(value);
-        setValue('');
-    }
-
-    return (
-        <form onSubmit={handleSubmit}>
-        <input 
-            type='text' 
-            className="input" 
-            value={value} 
-            placeholder="Add Todo..." 
-            onChange={e => setValue(e.target.value)} >
-        </form>
-        );
+  return (
+    <form onSubmit={handleSubmit}>
+      <input 
+        type="text"
+        className="input"
+        value={value}
+        placeholder="Add Todo..."
+        onChange={e => setValue(e.target.value)} />
+    </form>
+  )
 }
